06674d91ce220c781fe783c0113619e28c9fbe6e
Seulbee <doboo57@naver.com>
2023-08-13 16:43:02 +0900

    branch

---
+++ seulbee/java/java1/src/Example1.java
@@ -0,0 +1,6 @@
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("나는 소년이오 너는 소녀이다.");
+        System.out.println("I'm a boy! you are a girl");
+    }
+}
---
+++ seulbee/java/java2/pro01/src/com/happy/Example2.java
@@ -0,0 +1,10 @@
+package com.happy;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		System.out.println("두 번째 프로젝트");
+		
+	}
+
+}
